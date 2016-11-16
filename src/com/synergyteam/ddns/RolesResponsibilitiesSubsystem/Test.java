344192fda25830fd5673ae6af5c3736c24cfe25d
ol-coyote <racereddy20@gmail.com>
2016-11-16 01:33:48 -0700

    Implemented for loops to test values and HashMap

--- src/com/synergyteam/ddns/RolesResponsibilitiesSubsystem/Test.java
+++ src/com/synergyteam/ddns/RolesResponsibilitiesSubsystem/Test.java
@@ -1,20 +1,45 @@
 package com.synergyteam.ddns.RolesResponsibilitiesSubsystem;
 
 import java.util.HashMap;
+import java.util.Map;
+
 
 /**
  * Created by Paydro on 11/15/16.
  */
 public class Test {
     public static void main(String[] arg){
+        /* create RoleAccessManager object for testing */
         RoleAccessManager rrm = new RoleAccessManager();
-        System.out.println(rrm.associatePermissions(Roles.PA));
-        HashMap testMap = new HashMap();
+
+        /* Create HashMap to store names with permissions can change to id later*/
+        HashMap <String, Permissions> testMap = new <String, Permissions> HashMap();
+
+        /* Inserting bogus values with roles */
         testMap.put("Steve", rrm.associatePermissions(Roles.AE));
         testMap.put("Gator", rrm.associatePermissions(Roles.PI));
         testMap.put("Juan", rrm.associatePermissions(Roles.PA));
-        System.out.println("Steves's Permissions: " + testMap.get("Steve").toString());
-        System.out.println("Gator's Permissions: " + testMap.get("Gator").toString());
-        System.out.println("Juans's Permissions: " + testMap.get("Juan").toString());
+
+        System.out.println("*** Permissions within the ACCESS system ****");
+        System.out.println();
+         /* Iterate through permissions to see values */
+        for(Permissions p : Permissions.values())
+            System.out.printf("Permissions: %s %d \n", p.toString(), p.ordinal() );
+
+
+        System.out.println("\n*** Roles within the ACCESS system ****");
+        System.out.println();
+        /* Iterate through roles to see values */
+        for(Roles r : Roles.values())
+            System.out.printf("Roles: %s %d \n", r.toString(), r.ordinal() );
+
+        System.out.println("\n*** Insertions within the HashMap system ****");
+        System.out.println();
+        /* Iterate through Hashmap to see insertions */
+        for(String name: testMap.keySet()){
+            String key = name.toString();
+            Permissions p = testMap.get(name);
+            System.out.println(name +  " permissions: " + p);
+        }
     }
 }
