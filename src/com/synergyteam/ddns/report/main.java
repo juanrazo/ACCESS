d85ea905f94a906242088cf5d7ab934e32ce9b1f
musaric <carlos220594l@gmail.com>
2016-12-09 16:58:23 -0700

    Add files via upload

---
+++ src/com/synergyteam/ddns/report/main.java
@@ -0,0 +1,9 @@
+package reports;
+import java.util.*;
+import java.util.Date;
+public class main {
+
+	public static void main (String [] args){
+		System.out.println("this is working");
+	}
+}
---
+++ src/com/synergyteam/ddns/report/summaryReport.java
@@ -0,0 +1,26 @@
+package reports;
+
+public class summaryReport {
+
+	public void getResource(){
+
+	}
+
+	public void getInitiative(){
+
+	}
+	
+	public void getActivity(){
+
+	}
+
+	public void getCategory(){
+
+	}
+	
+	public void getUserInformation(){
+
+	}
+	
+	
+}
