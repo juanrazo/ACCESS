44a5a0ad743fb88ce481dc0569fe41f544d78980
musaric <carlos220594l@gmail.com>
2016-12-09 22:32:16 -0700

    Update summaryReport.java

--- src/com/synergyteam/ddns/report/summaryReport.java
+++ src/com/synergyteam/ddns/report/summaryReport.java
@@ -3,23 +3,23 @@ package reports;
 public class summaryReport {
 
 	public void getResource(){
-
+	   return resource;
 	}
 
 	public void getInitiative(){
-
+           return initiative;
 	}
 	
 	public void getActivity(){
-
+	   return avtivity;
 	}
 
 	public void getCategory(){
-
+          return reportCategory;
 	}
 	
 	public void getUserInformation(){
-
+	   return participanInformation;
 	}
 	
 	
